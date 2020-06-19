b673daef71d50daf9e78ac1282b468d4dd854e3c
Alecto <perez.cs@pm.me>
2020-06-19 15:31:05 -0600

    Added flag to change behavior of initial suspend

--- include/generator_promise.hpp
+++ include/generator_promise.hpp
@@ -1,20 +1,22 @@
 #pragma once
 #include <common.hpp>
 
-template <class T, template<class> class return_object_t, bool is_coro_noexcept = true>
+template <class T, template<class> class ReturnObject_t, bool IsNoexcept = true, bool SuspendInitially = false>
 struct generator_promise {
    private:
     static T& mutable_T();
     static T const& const_T();
    public:
-    constexpr static bool is_noexcept = is_coro_noexcept;
+    constexpr static bool is_noexcept = IsNoexcept;
+    // true if initial_suspend() returns suspend_always
+    constexpr static bool suspend_initially = SuspendInitially;
     constexpr static bool copy_T_noexcept =
         is_noexcept || noexcept(T(const_T()));
     constexpr static bool assign_T_noexcept =
         is_noexcept || noexcept(mutable_T() = const_T());
 
     using handle = std::coroutine_handle<generator_promise>;
-    using return_object = return_object_t<generator_promise>;
+    using return_object = ReturnObject_t<generator_promise>;
     T current_value;
     static auto get_return_object_on_allocation_failure() noexcept {
         return return_object{nullptr};
@@ -22,7 +24,13 @@ struct generator_promise {
     auto get_return_object() noexcept {
         return return_object{handle::from_promise(*this)};
     }
-    constexpr auto initial_suspend() noexcept { return std::suspend_always(); }
+    constexpr auto initial_suspend() noexcept { 
+        if constexpr(SuspendInitially) {
+            return std::suspend_always(); 
+        } else {
+            return std::suspend_never();
+        }
+    }
     constexpr auto final_suspend() noexcept { return std::suspend_always(); }
     [[noreturn]] void unhandled_exception() noexcept { std::terminate(); }
     constexpr void return_void() noexcept {}
