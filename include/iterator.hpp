810e24e32dc0c0bd4e8cf8a5bda7f1b48b9d3ba8
Alecto <perez.cs@pm.me>
2020-06-19 15:32:15 -0600

    Added comments

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -1,3 +1,4 @@
+#pragma once
 
 struct coro_sentinal {};
 
@@ -8,19 +9,28 @@ using deref_current_value_t = decltype(deref_current_value);
 template <class handle, class deref_fn = deref_current_value_t, bool is_noexcept = true>
 struct coro_iterator {
     handle coro = nullptr;
+    // Used to implement dereference
     [[no_unique_address]] deref_fn deref = {};
 
+    // Resumes the coroutine; returns reference to this iterator
     auto operator++() noexcept(is_noexcept) -> coro_iterator& {
         coro.resume();
         return *this;
     }
+    // Resumes the coroutine
     void operator++(int) noexcept(is_noexcept) { coro.resume(); }
+
+    // Calls deref on the coroutine handle and returns the result
+    // By default, this returns coro.promise().current_value;
     decltype(auto) operator*() noexcept(noexcept(deref(coro))) {
         return deref(coro);
     }
+
+    // Returns true iff the coroutine is done
     bool operator==(coro_sentinal) noexcept(is_noexcept) {
         return coro.done();
     }
+    // Returns true iff the coroutine is not done
     bool operator!=(coro_sentinal) noexcept(is_noexcept) {
         return !coro.done();
     }
