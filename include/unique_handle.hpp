f22de6390055193b49610795eb05fc4a86fc73fa
Alecto <perez.cs@pm.me>
2020-06-19 13:27:30 -0600

    Added release(), reset(), get(), and operator->()

--- include/unique_handle.hpp
+++ include/unique_handle.hpp
@@ -3,26 +3,37 @@
 template<class Promise>
 struct unique_handle : private std::coroutine_handle<Promise> {
     using promise_type = Promise;
-    using handle_type = std::coroutine_handle<Promise>;
-    using handle_type::operator bool;
-    using handle_type::done;
-    using handle_type::resume;
-    using handle_type::promise;
-    using handle_type::from_promise;
-    using handle_type::from_address;
+    using pointer = std::coroutine_handle<Promise>;
+
+    // Returns true if the coroutine handle isn't null
+    using pointer::operator bool;
+    // Checks if the coroutine has finished executing
+    using pointer::done;
+    // Resumes execution of the coroutine
+    using pointer::resume;
+    // Obtains the promise object
+    using pointer::promise;
+    // Obtains the coroutine handle as a void*
+    using pointer::address;
+
+    // Creates a unique_handle from the promise_type object in the coroutine_frame
+    static unique_handle from_promise(promise_type& p) {
+        return unique_handle(pointer::from_promise(p)); 
+    }
+    // Creates a unique_handle from a void* representing a coroutine_handle
+    static unique_handle from_address(void* addr) {
+        return unique_handle(pointer::from_address(addr)); 
+    }
+
     
-    
-    explicit unique_handle(handle_type handle) : handle_type(handle) {}
+    explicit unique_handle(pointer handle) : pointer(handle) {}
     unique_handle(unique_handle const&) = delete;
-    unique_handle(unique_handle&& source) : handle_type(source) {
-        source.handle_type::operator=(nullptr); 
+    unique_handle(unique_handle&& source) : pointer(source) {
+        source.pointer::operator=(nullptr); 
     }
 
     unique_handle& operator=(std::nullptr_t) {
-        if(*this) {
-            handle_type::destroy();
-            handle_type::operator=(nullptr);
-        }
+        reset(); 
         return *this;
     }
     unique_handle& operator=(unique_handle&& other) {
@@ -31,21 +42,41 @@ struct unique_handle : private std::coroutine_handle<Promise> {
         swap(tmp); 
     }
 
-    handle_type detach() {
-        handle_type h = *this;
-
+    pointer release() {
+        pointer h = *this;
+        *this = nullptr; 
+        return h;
+    }
+    void reset(std::nullptr_t = nullptr) {
+        if(*this) {
+            pointer::destroy();
+            pointer::operator=(nullptr);
+        }
+    }
+    void reset(pointer p) {
+        if(*this) {
+            pointer::destroy();
+            pointer::operator=(p);
+        }
     }
     void swap(unique_handle& other) noexcept {
-        handle_type tmp = *this;
-        handle_type::operator=(other);
-        other.handle_type::operator=(tmp); 
+        pointer tmp = *this;
+        pointer::operator=(other);
+        other.pointer::operator=(tmp); 
+    }
+    pointer get() {
+        return *this;
     }
     void destroy() {
-        *this = nullptr; 
+        reset(); 
     }
     ~unique_handle() {
         if(*this) {
-            handle_type::destroy(); 
+            pointer::destroy(); 
         }
+    }
+    // Accesses the promise_type
+    promise_type* operator->() {
+        return &pointer::promise(); 
     }
 };
