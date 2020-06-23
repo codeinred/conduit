dae1556517d047c8281b6f715dc0d11837eff11b
Alecto <perez.cs@pm.me>
2020-06-23 00:03:01 -0600

    Added example showing channel usage

--- example.cpp
+++ example.cpp
@@ -1,10 +1,12 @@
 #include "include/generator_promise.hpp"
 #include "include/unique_handle.hpp"
-#include <iostream>
+#include <channel.hpp>
 #include <generator.hpp>
+#include <iostream>
 
-template<bool is_suspended_initially = check_first>
-generator<int, true, is_suspended_initially> counter(int initial, int max, int inc) {
+template <bool is_suspended_initially = check_first>
+generator<int, true, is_suspended_initially> counter(int initial, int max,
+                                                     int inc) {
     for (; initial < max; initial += inc) {
         co_yield initial;
     }
@@ -19,7 +21,7 @@ void example1() {
 }
 void example2() {
     auto countBy3 = counter<resume_first>(0, 30, 3);
-    for(countBy3.resume(); !countBy3.done(); countBy3.resume()) {
+    for (countBy3.resume(); !countBy3.done(); countBy3.resume()) {
         std::cout << countBy3->value << ' ';
     }
     std::cout << '\n';
@@ -27,7 +29,7 @@ void example2() {
 void example3() {
     auto countBy3 = counter<check_first>(0, 30, 3);
     int value = 0;
-    while(countBy3 >> value) {
+    while (countBy3 >> value) {
         std::cout << value << ' ';
     }
     std::cout << '\n';
@@ -35,7 +37,7 @@ void example3() {
 void example4() {
     auto countBy3 = counter<resume_first>(0, 30, 3);
     int value = 0;
-    while(countBy3 >> value) {
+    while (countBy3 >> value) {
         std::cout << value << ' ';
     }
     std::cout << '\n';
@@ -51,10 +53,25 @@ void example6() {
     auto countBy3 = counter(0, 30, 3);
     auto it = begin(countBy3);
     auto sentinal = end(countBy3);
-    for(; it != sentinal; ++it) {
+    for (; it != sentinal; ++it) {
         std::cout << *it << ' ';
     }
     std::cout << '\n';
+}
+
+channel<int> sum_messages() {
+    int sum = co_yield get_message;
+    while(true) {
+        sum += co_yield sum;
+    }
+}
+void example_channel() {
+    auto ch = sum_messages();
+    for(int i = 0; i <= 10; i++) {
+        int result;
+        ch << i >> result;
+        std::cout << "Sum so far: " << result << '\n';
+    }
 }
 int main() {
     example1();
@@ -63,4 +80,5 @@ int main() {
     example4();
     example5();
     example6();
+    example_channel();
 }
