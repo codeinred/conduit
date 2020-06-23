3acd251bb6d2ebf1a5896e0c49a64d3c291ea331
Alecto <perez.cs@pm.me>
2020-06-22 22:31:35 -0600

    Added template deduction guide

--- include/awaitable_reference.hpp
+++ include/awaitable_reference.hpp
@@ -8,3 +8,5 @@ struct awaitable_reference {
     void await_suspend(std::coroutine_handle<>) {}
     T await_resume() { return value; }
 };
+template<class T>
+awaitable_reference(T&) -> awaitable_reference<T>;
