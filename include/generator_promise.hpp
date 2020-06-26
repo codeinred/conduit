42f75bd02687f356c5d6888cf4b21255f9ecef0b
Alecto <perez.cs@pm.me>
2020-06-26 11:55:23 -0600

    Greatly simplified generator_promise implementanion
    We used promise_base to greatly simplify the implementation of generator_promise

--- include/generator_promise.hpp
+++ include/generator_promise.hpp
@@ -1,54 +1,43 @@
 #pragma once
-#include <common.hpp>
+#include <promise_base.hpp>
 
 enum generator_mode : bool { check_first = false, resume_first = true };
 template <
     // Type output by generator
     class T,
     // Template to make return object
-    template <class> class ReturnObject_t,
+    template <class> class ReturnObject,
     // Should funcitons be noexcept
     bool IsNoexcept = true,
     // Should the coroutine always suspend initially
     bool SuspendInitially = check_first>
-struct generator_promise {
-    using handle = std::coroutine_handle<generator_promise>;
-    using return_object = ReturnObject_t<generator_promise>;
-
+struct generator_promise
+    : promise_base<generator_promise<T, ReturnObject, IsNoexcept, SuspendInitially>, ReturnObject> {
     // yielded value stored here
     T value;
 
-    static auto get_return_object_on_allocation_failure() noexcept {
-        return return_object{nullptr};
+    // If SuspendsInitially is false, we hide the default behavior in
+    // promise_base
+    constexpr auto initial_suspend() noexcept requires(!SuspendInitially) {
+        return std::suspend_never();
     }
-    auto get_return_object() noexcept {
-        return return_object{handle::from_promise(*this)};
-    }
-    constexpr auto initial_suspend() noexcept {
-        if constexpr (suspend_initially) {
-            return std::suspend_always();
-        } else {
-            return std::suspend_never();
-        }
-    }
-    constexpr auto final_suspend() noexcept { return std::suspend_always(); }
-    [[noreturn]] void unhandled_exception() noexcept { std::terminate(); }
-    constexpr void return_void() noexcept {}
-    constexpr auto yield_value(T value) noexcept(assign_T_noexcept) {
+
+    // Stores value in this->value, to be accessed by the caller via
+    // coroutine_handle.promise()
+    constexpr auto yield_value(T value) noexcept(move_T_noexcept) {
         this->value = std::move(value);
         return std::suspend_always{};
     }
 
+    // true if template noexcept flag is marked true
     constexpr static bool is_noexcept = IsNoexcept;
+
     // true if initial_suspend() returns suspend_always
     constexpr static bool suspend_initially = SuspendInitially;
 
    private:
     // These are just used to get a reference to T in template expressions
     static T& mutable_T();
-    static T const& const_T();
-    constexpr static bool copy_T_noexcept =
-        is_noexcept || noexcept(T(const_T()));
-    constexpr static bool assign_T_noexcept =
-        is_noexcept || noexcept(mutable_T() = const_T());
+    static T&& moved_T();
+    constexpr static bool move_T_noexcept = is_noexcept || noexcept(mutable_T() = moved_T());
 };
