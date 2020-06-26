64e8484d597e47eb1ea4d7619c46024b0ff463ae
Alecto <perez.cs@pm.me>
2020-06-26 14:52:14 -0600

    Removed unnecessary tag_type base
    It turns out that we can replicate the necessary functionality by making the default constructor explicit, eliminating the need for tag_type::make() or private constructors (oops)

--- include/tag_types.hpp
+++ include/tag_types.hpp
@@ -1,18 +1,7 @@
 #pragma once
-// This provides boilerplate to make a minimal tag_type
-// With the ability to prevent ambiguity in overload resolution
-template<class tag>
-struct tag_type {
-    tag_type(tag_type const&) = default;
-    constexpr static tag make() {
-        return tag();
-    }
-    // Making tag_type explicit prevents ambiguity in overload resolution
-    explicit tag_type() = default;
-};
 
-struct get_promise_t : tag_type<get_promise_t> {};
-constexpr get_promise_t get_promise = get_promise_t();
+struct get_promise_t { explicit get_promise_t() = default; };
+constexpr auto get_promise = get_promise_t();
 
-struct get_message_t : tag_type<get_message_t> {};
-constexpr get_message_t get_message = get_message_t();
+struct get_message_t { explicit get_message_t() = default; };
+constexpr auto get_message = get_message_t();
