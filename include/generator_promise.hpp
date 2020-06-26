28551094122dd5885a924cdb224800c12b85e1d2
Alecto <perez.cs@pm.me>
2020-06-26 12:48:00 -0600

    Removed superfluous initial_suspend()
    This functionality was moved into promise_base via the SuspendInitially template option

--- include/generator_promise.hpp
+++ include/generator_promise.hpp
@@ -12,18 +12,10 @@ template <
     // Should the coroutine always suspend initially
     bool SuspendInitially = check_first>
 struct generator_promise
-    : promise_base<generator_promise<T, ReturnObject, IsNoexcept, SuspendInitially>, ReturnObject> {
+    : promise_base<generator_promise<T, ReturnObject, IsNoexcept, SuspendInitially>, ReturnObject,
+                   SuspendInitially> {
     // yielded value stored here
     T value;
-
-    // If SuspendsInitially is false, we hide the default behavior in
-    // promise_base
-    constexpr auto initial_suspend() noexcept {
-        if constexpr (SuspendInitially)
-            return std::suspend_always{};
-        else
-            return std::suspend_never{};
-    }
 
     // Stores value in this->value, to be accessed by the caller via
     // coroutine_handle.promise()
