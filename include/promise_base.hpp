84afe4c34ea8496f32d3f27f576e5d9a4615d10d
Alecto <perez.cs@pm.me>
2020-06-26 00:38:07 -0600

    Fixed promise_base implementation
    promise_base had a bug where it depended on a definition in the derived class. Fixed this by providing a template argument for return_object.

--- include/promise_base.hpp
+++ include/promise_base.hpp
@@ -1,22 +1,19 @@
 #pragma once
 #include <common.hpp>
-
-template <class Derived>
-struct promise_base;
+#include <unique_handle.hpp>
 
 // This is an incomplete promise_base
-template <>
-struct promise_base<void> {
+struct promise_base_base {
     constexpr auto initial_suspend() noexcept { return std::suspend_always(); }
     constexpr auto final_suspend() noexcept { return std::suspend_always(); }
     [[noreturn]] void unhandled_exception() noexcept { std::terminate(); }
     constexpr void return_void() noexcept {}
 };
 
-template <class Derived>
-struct promise_base : promise_base<void> {
-    using handle = typename Derived::handle;
-    using return_object = typename Derived::return_object;
+template <class Derived, template <class> class ReturnObject = unique_handle>
+struct promise_base : promise_base_base {
+    using handle = std::coroutine_handle<Derived>;
+    using return_object = ReturnObject<Derived>;
 
     Derived& get_promise() { return static_cast<Derived&>(*this); }
     static auto get_return_object_on_allocation_failure() noexcept {
