fd2a19f1eebd84c6a82758885e49a19666e24c2a
Alecto <perez.cs@pm.me>
2020-06-26 12:53:25 -0600

    Greatly simplified channel_promise implementation
    We did the same thing as with generator_promise where we eliminated boilerplate by using promise_base

--- include/channel_promise.hpp
+++ include/channel_promise.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "generator.hpp"
 #include <awaitable_reference.hpp>
+#include <generator_promise.hpp>
 
 struct get_message_t {};
 constexpr get_message_t get_message = {};
@@ -9,44 +9,27 @@ template <
     // Type output by generator
     class T, class Message,
     // Template to make return object
-    template <class> class ReturnObject_t,
+    template <class> class ReturnObject,
     // Should funcitons be noexcept
     bool IsNoexcept = true,
     // Should the coroutine always suspend initially
     bool SuspendInitially = check_first>
-struct channel_promise {
-    using handle = std::coroutine_handle<channel_promise>;
-    using return_object = ReturnObject_t<channel_promise>;
+struct channel_promise
+    : promise_base<channel_promise<T, Message, ReturnObject, IsNoexcept, SuspendInitially>,
+                   ReturnObject, SuspendInitially> {
     using message_type = Message;
 
     // yielded value stored here
     T value = {};
+
     // message stored here
     Message message = {};
 
-    static auto get_return_object_on_allocation_failure() noexcept {
-        return return_object{nullptr};
-    }
-    auto get_return_object() noexcept {
-        return return_object{handle::from_promise(*this)};
-    }
-    constexpr auto initial_suspend() noexcept {
-        if constexpr (SuspendInitially) {
-            return std::suspend_always();
-        } else {
-            return std::suspend_never();
-        }
-    }
-    constexpr auto final_suspend() noexcept { return std::suspend_always(); }
-    [[noreturn]] void unhandled_exception() noexcept { std::terminate(); }
-    constexpr void return_void() noexcept {}
-    constexpr auto yield_value(T value) noexcept(assign_T_noexcept) {
+    constexpr auto yield_value(T value) noexcept(move_T_noexcept) {
         this->value = std::move(value);
         return awaitable_reference{message};
     }
-    constexpr auto yield_value(get_message_t) {
-        return awaitable_reference{message};
-    }
+    constexpr auto yield_value(get_message_t) { return awaitable_reference{message}; }
 
     constexpr static bool is_noexcept = IsNoexcept;
     // true if initial_suspend() returns suspend_always
@@ -55,9 +38,6 @@ struct channel_promise {
    private:
     // These are just used to get a reference to T in template expressions
     static T& mutable_T();
-    static T const& const_T();
-    constexpr static bool copy_T_noexcept =
-        is_noexcept || noexcept(T(const_T()));
-    constexpr static bool assign_T_noexcept =
-        is_noexcept || noexcept(mutable_T() = const_T());
+    static T&& moved_T();
+    constexpr static bool move_T_noexcept = is_noexcept || noexcept(mutable_T() = moved_T());
 };
