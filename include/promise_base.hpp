6722de3a95df7fae88dff0dca445ef712357ebd8
Alecto <perez.cs@pm.me>
2020-06-26 12:43:47 -0600

    Added SuspendInitially + cleaned code
    Use of a template flag for SuspendInitially is a common pattern in other parts of the codebase, and can be safely abstracted into promise_base to reduce boilerplate code.

--- include/promise_base.hpp
+++ include/promise_base.hpp
@@ -3,23 +3,40 @@
 #include <unique_handle.hpp>
 
 // This is an incomplete promise_base
+template <bool SuspendInitially>
 struct promise_base_base {
-    constexpr auto initial_suspend() noexcept { return std::suspend_always(); }
+    constexpr auto initial_suspend() noexcept {
+        if constexpr (SuspendInitially)
+            return std::suspend_always{};
+        else
+            return std::suspend_never{};
+    }
     constexpr auto final_suspend() noexcept { return std::suspend_always(); }
     [[noreturn]] void unhandled_exception() noexcept { std::terminate(); }
     constexpr void return_void() noexcept {}
 };
 
-template <class Derived, template <class> class ReturnObject = unique_handle>
-struct promise_base : promise_base_base {
+template <
+    // This is the class that derives from promise_base.
+    class Derived,
+    // This is the ReturnObject template that's instantiated with Derived
+    template <class> class ReturnObject = unique_handle,
+    // And this determines whether or not initial_suspend returns
+    // suspend_always or suspend_never
+    bool SuspendInitially = true>
+struct promise_base : promise_base_base<SuspendInitially> {
     using handle = std::coroutine_handle<Derived>;
     using return_object = ReturnObject<Derived>;
 
-    Derived& get_promise() { return static_cast<Derived&>(*this); }
+    // If there's an allocation failure, returns a null coroutine handle
     static auto get_return_object_on_allocation_failure() noexcept {
         return return_object{nullptr};
     }
-    auto get_return_object() noexcept {
-        return return_object{handle::from_promise(get_promise())};
-    }
+
+    // Used by the compiler to produce the return_object when starting the coroutine
+    auto get_return_object() noexcept { return return_object{handle::from_promise(get_promise())}; }
+
+   private:
+    // Obtains the promise object from this
+    Derived& get_promise() { return static_cast<Derived&>(*this); }
 };
