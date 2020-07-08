97b7ad2c1b18d87cd3ab8e5f3f7a2722be751478
Alecto <perez.cs@pm.me>
2020-07-08 14:45:39 -0600

    Formatting

--- include/awaitable_callback.hpp
+++ include/awaitable_callback.hpp
@@ -3,11 +3,7 @@
 
 struct awaitable_callback {
     std::coroutine_handle<> callback = std::noop_coroutine();
-    constexpr bool await_ready() noexcept {
-        return false;
-    }
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept {
-        return callback;
-    }
+    constexpr bool await_ready() noexcept { return false; }
+    auto await_suspend(std::coroutine_handle<>) noexcept { return callback; }
     void await_resume() noexcept {}
 };
