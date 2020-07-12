2f419d04329062f33db1523f4db90181913ffcec
Alecto <perez.cs@pm.me>
2020-07-11 22:58:47 -0600

    added promise_t, handle_t, and can_co_return

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -1,12 +1,23 @@
 #pragma once
 #include <coroutine>
 
-template<class Promise, class ReturnObject>
+template <class T>
+using promise_t = typename std::coroutine_traits<T>::promise_type;
+
+template <class T>
+using handle_t = std::coroutine_handle<promise_t<T>>;
+
+template <class Promise, class ReturnObject>
 concept return_object_aware = requires(Promise promise, ReturnObject* r) {
-    { promise.set_return_object(r) };
+    {promise.set_return_object(r)};
 };
 
-template<class Coro>
-concept coroutine_type = requires(typename std::coroutine_traits<Coro>::promise_type promise) {
-    { Coro(promise) };
+template <class Coro>
+concept coroutine_type = requires(handle_t<Coro> handle) {
+    {Coro(handle)};
+};
+
+template <class Coro, class type>
+concept can_co_return = coroutine_type<Coro>&& requires(promise_t<Coro> promise, type t) {
+    promise.return_value(t);
 };
