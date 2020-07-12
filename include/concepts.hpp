07e5da329b0f4a134c3922e911f2f3c57b0a03ad
Alecto <perez.cs@pm.me>
2020-07-11 22:47:43 -0600

    Added coroutine_type concept

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -1,6 +1,12 @@
 #pragma once
+#include <coroutine>
 
 template<class Promise, class ReturnObject>
 concept return_object_aware = requires(Promise promise, ReturnObject* r) {
     { promise.set_return_object(r) };
+};
+
+template<class Coro>
+concept coroutine_type = requires(typename std::coroutine_traits<Coro>::promise_type promise) {
+    { Coro(promise) };
 };
