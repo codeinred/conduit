ff1a305f749cbec97749c91aae770669319615e0
Alecto <perez.cs@pm.me>
2020-07-17 22:35:59 -0600

    Updated future to use temporary_handle

--- include/conduit/promise/future.hpp
+++ include/conduit/promise/future.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include <conduit/async/jump.hpp>
 #include <conduit/promise/helper.hpp>
+#include <conduit/temporary_handle.hpp>
 
 namespace conduit::promise {
 template <class ReturnValue>
 struct future : helper<future<ReturnValue>, suspend_initially> {
-    std::coroutine_handle<> callback = std::noop_coroutine();
+    temporary_handle callback = std::noop_coroutine();
     ReturnValue returned_value = {};
 
-    auto final_suspend() noexcept { return async::jump{callback}; }
+    auto final_suspend() noexcept { return async::jump{callback.release()}; }
     void set_callback(std::coroutine_handle<> callback) noexcept { this->callback = callback; }
     void return_value(ReturnValue value) { returned_value = std::move(value); }
     ReturnValue get_value() { return std::move(returned_value); }
