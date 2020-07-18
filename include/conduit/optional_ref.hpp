a46ab8f6193018b3782da09ea5ec60b10c47257e
Alecto <perez.cs@pm.me>
2020-07-17 22:36:45 -0600

    Implemented optional_ref

---
+++ include/conduit/optional_ref.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <concepts>
+#include <cstddef>
+#include <utility>
+
+namespace conduit {
+template <class T>
+class optional_ref {
+    T* pointer = nullptr;
+   public:
+    optional_ref() = default;
+    optional_ref(optional_ref const&) = default;
+    optional_ref(optional_ref&&) = default;
+    constexpr optional_ref(T& value) : pointer(&value) {}
+    constexpr optional_ref(std::nullptr_t) : pointer(nullptr) {}
+    constexpr T* operator->() const noexcept { return pointer; }
+    constexpr T& operator*() const& noexcept { return *pointer; }
+    constexpr T&& operator*() && noexcept { return std::move(*pointer); }
+    constexpr T& value() const& noexcept { return *pointer; }
+    constexpr T&& value() && noexcept { return std::move(*pointer); }
+    constexpr operator bool() const noexcept { return pointer != nullptr; }
+    constexpr bool has_value() const noexcept { return pointer != nullptr; }
+    template <std::convertible_to<T> U>
+    constexpr T value_or(U&& default_value) const& {
+        return pointer ? T(*pointer) : T(std::forward<U>(default_value));
+    }
+    template <std::convertible_to<T> U>
+    constexpr T value_or(U&& default_value) && {
+        return pointer ? T(std::move(*pointer)) : T(std::forward<U>(default_value));
+    }
+};
+} // namespace conduit
