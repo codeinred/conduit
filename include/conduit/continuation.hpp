40fd3e08afea83c108d6703336eedf3e66ba40c2
Alecto <perez.cs@pm.me>
2020-08-01 17:27:17 -0600

    Added noop_continuation_size
    This is the amount of space taken up by a noop_continuation when it allocates. It's on a compiler basis

--- include/conduit/continuation.hpp
+++ include/conduit/continuation.hpp
@@ -29,4 +29,12 @@ template <class Alloc>
 continuation<Alloc> noop_continuation(Alloc& alloc) {
     co_return;
 }
+
+// On x86-64, noop_continuation_size is 24 on Clang
+// And 48 on GCC. I don't know what it is on other compilers
+#ifdef __clang__
+constexpr size_t noop_continuation_size = sizeof(void*) * 3;
+#else
+constexpr size_t noop_continuation_size = sizeof(void*) * 6;
+#endif
 } // namespace conduit
