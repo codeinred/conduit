406e6672bbe98cf50675c25384127a71e0121b53
Alecto <perez.cs@pm.me>
2020-08-01 15:10:25 -0600

    Cleaned up continuation implementation

--- include/conduit/continuation.hpp
+++ include/conduit/continuation.hpp
@@ -1,2 +1,32 @@
 #pragma once
 #include <conduit/promise/continuation.hpp>
+
+namespace conduit {
+template <class Promise>
+struct continuation : std::coroutine_handle<Promise> {
+    using promise_type = Promise;
+    using super = std::coroutine_handle<Promise>;
+
+/*
+Clang requires that a return object has a constructor which allows it to be
+constructed from a handle, but having this constructor causes an internal
+compiler error in gcc
+*/
+#ifdef __clang__
+    continuation() = default;
+    continuation(super handle) : super(handle) {}
+    continuation(continuation const&) = default;
+    continuation(continuation&&) = default;
+#endif
+};
+
+template <conduit::allocator Alloc>
+continuation<promise::continuation<Alloc>> noop_continuation(Alloc* alloc) {
+    co_return;
+}
+
+template <class Alloc>
+continuation<promise::continuation<Alloc>> noop_continuation(Alloc& alloc) {
+    co_return;
+}
+} // namespace conduit
--- include/conduit/promise/continuation.hpp
+++ include/conduit/promise/continuation.hpp
@@ -1,34 +1,14 @@
 #pragma once
-#include <conduit/promise/helper.hpp>
-#include <cstdio>
-
-namespace conduit {
-template <class Alloc>
-concept allocator = requires(Alloc a, size_t size, void* pointer) {
-    { a.alloc(size) }
-    ->std::same_as<void*>;
-    { Alloc::dealloc(pointer, size) }
-    ->std::same_as<void>;
-};
-template <class Promise>
-struct continuation : std::coroutine_handle<Promise> {
-    using promise_type = Promise;
-};
-} // namespace conduit
+#include <conduit/concepts.hpp>
+#include <conduit/mixin/promise_parts.hpp>
 
 namespace conduit::promise {
 template <conduit::allocator Alloc>
-struct continuation : initial_suspend_base<true>,
-                      final_suspend_base<false>,
-                      return_void_base<true> {
-    using self = continuation;
-    using result_type = conduit::continuation<self>;
-    using handle_type = std::coroutine_handle<self>;
-    result_type get_return_object() noexcept {
-        puts("Getting return object");
-        return {handle_type::from_promise(*this)};
-    }
-    void unhandled_exception() {}
+struct continuation : mixin::GetReturnObject<continuation<Alloc>, false>,
+                      mixin::UnhandledException<true>,
+                      mixin::InitialSuspend<mixin::always>,
+                      mixin::FinalSuspend<mixin::never>,
+                      mixin::ReturnVoid {
     template <class... T>
     static void* operator new(size_t size, Alloc& alloc, T&&...) {
         return alloc.alloc(size);
@@ -38,19 +18,7 @@ struct continuation : initial_suspend_base<true>,
         return alloc->alloc(size);
     }
     static void operator delete(void* pointer, size_t size) {
-        puts("Calling delete");
         std::decay_t<Alloc>::dealloc(pointer, size);
     }
 };
 } // namespace conduit::promise
-
-namespace conduit {
-template<class Alloc>
-continuation<promise::continuation<Alloc>> noop_continuation(Alloc* alloc) {
-    co_return;
-}
-template<class Alloc>
-continuation<promise::continuation<Alloc>> noop_continuation(Alloc& alloc) {
-    co_return;
-}
-}
