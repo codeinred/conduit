375aee1dda27fef4092f301b6169ea2e753da4b3
Alecto <perez.cs@pm.me>
2020-08-02 00:58:38 -0600

    Simplified via mixins

--- include/conduit/promise/future.hpp
+++ include/conduit/promise/future.hpp
@@ -1,20 +1,15 @@
 #pragma once
-#include <conduit/async/jump.hpp>
+#include <conduit/mixin/promise_parts.hpp>
 #include <conduit/optional_ref.hpp>
-#include <conduit/promise/helper.hpp>
-#include <conduit/temporary_handle.hpp>
-
 #include <optional>
 
 namespace conduit::promise {
 template <class ReturnValue>
-struct future : helper<future<ReturnValue>, traits<true, true, false, true>> {
-    temporary_handle callback = std::noop_coroutine();
+struct future : mixin::GetReturnObject<future<ReturnValue>>,
+                mixin::PromiseWithCallback,
+                mixin::UnhandledException<true> {
     std::optional<ReturnValue> result;
-
-    auto final_suspend() noexcept { return async::jump{callback.release()}; }
-    void set_callback(std::coroutine_handle<> callback) noexcept { this->callback = callback; }
-    void return_value(ReturnValue result) { this->result.emplace(std::move(result)); }
+    void return_value(ReturnValue r) { result.emplace(std::move(r)); }
     void return_value(nothing_t) {}
     optional_ref<ReturnValue> get_value() & { return optional_ref(result); }
     std::optional<ReturnValue> get_value() && { return std::move(result); }
