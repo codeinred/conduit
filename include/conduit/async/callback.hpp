8dbfd311ab2e8a89da798c1c55e485d8c2036e3c
Alecto <perez.cs@pm.me>
2020-08-02 00:42:55 -0600

    Fixed constexpr usage + added release_jump()

--- include/conduit/async/callback.hpp
+++ include/conduit/async/callback.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <conduit/async/jump.hpp>
 #include <conduit/common.hpp>
 #include <conduit/mixin/awaitable_parts.hpp>
 
@@ -7,7 +8,7 @@ namespace with_nullptr {
 // Implements async::callback using a default state of nullptr
 class callback : mixin::AwaitReady<false>, mixin::AwaitResume {
     std::coroutine_handle<> h = nullptr;
-    static constexpr inline auto release(std::coroutine_handle<>& h) noexcept
+    static inline auto release(std::coroutine_handle<>& h) noexcept
         -> std::coroutine_handle<> {
         auto copy = h;
         h = nullptr;
@@ -20,10 +21,15 @@ class callback : mixin::AwaitReady<false>, mixin::AwaitResume {
     constexpr callback(std::nullptr_t) noexcept : h(nullptr) {}
     constexpr callback(std::coroutine_handle<> h) noexcept : h(h) {}
     callback(const callback&) = delete;
-    constexpr callback(callback&& source) noexcept : h(release(source.h)) {}
+    callback(callback&& source) noexcept : h(release(source.h)) {}
 
     [[nodiscard]] auto release() noexcept -> callback {
         return callback(release(h));
+    }
+    // Releases ownership of the coroutine and produces an async::jump
+    // with that coroutine
+    [[nodiscard]] auto release_jump() noexcept -> jump {
+        return jump{h ? release(h) : std::noop_coroutine()};
     }
     auto operator=(callback source) noexcept -> callback& {
         swap(source);
@@ -46,7 +52,7 @@ class callback : mixin::AwaitReady<false>, mixin::AwaitResume {
     }
     using AwaitReady<false>::await_ready;
     using AwaitResume::await_resume;
-    constexpr auto await_suspend(std::coroutine_handle<>) noexcept
+    auto await_suspend(std::coroutine_handle<>) noexcept
         -> std::coroutine_handle<> {
         return h ? release(h) : std::noop_coroutine();
     }
@@ -59,7 +65,7 @@ class callback : mixin::AwaitReady<false>, mixin::AwaitResume {
     std::coroutine_handle<> h = std::noop_coroutine();
     // Bypasses M::sanitize, used by release() method
     constexpr callback(std::coroutine_handle<> h, int) noexcept : h(h) {}
-    static constexpr inline auto release(std::coroutine_handle<>& h) noexcept
+    static inline auto release(std::coroutine_handle<>& h) noexcept
         -> std::coroutine_handle<> {
         auto copy = h;
         h = std::noop_coroutine();
@@ -78,6 +84,7 @@ class callback : mixin::AwaitReady<false>, mixin::AwaitResume {
     [[nodiscard]] auto release() noexcept -> callback {
         return callback(release(h), 0);
     }
+    [[nodiscard]] auto release_jump() noexcept -> jump { return jump{release(h)}; }
     auto operator=(callback source) noexcept -> callback& {
         swap(source);
         return *this;
