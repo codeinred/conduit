6c91bb753abbdb6729ea9a4d136f2f21f50ad7f8
Alecto <perez.cs@pm.me>
2020-08-14 19:27:55 -0600

    CReated generic awaitable coroutine

---
+++ include/conduit/async/coro.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <conduit/concepts.hpp>
+#include <conduit/async/on_coro.hpp>
+#include <cstddef>
+
+namespace conduit::async {
+template <class Promise>
+class coro {
+    std::coroutine_handle<Promise> handle = nullptr;
+    std::coroutine_handle<Promise> release() {
+        auto hold = handle;
+        handle = nullptr;
+        return hold;
+    }
+
+   public:
+    using promise_type = Promise;
+    using handle_type = std::coroutine_handle<Promise>;
+
+    constexpr coro() = default;
+    coro(coro const&) = delete;
+    constexpr coro(std::nullptr_t) noexcept : coro() {}
+    coro(std::coroutine_handle<Promise> h) noexcept : handle(h) {
+        if(handle) {
+            handle.promise().set_owner(&handle);
+        }
+    }
+    constexpr coro(coro&& source) noexcept : handle(source.release()) {
+        if (handle) {
+            handle.promise().set_owner(&handle);
+        }
+    }
+
+    constexpr coro& operator=(coro c) noexcept {
+        swap(c);
+        return *this;
+    }
+
+    constexpr void swap(coro& other) noexcept {
+        std::swap(handle, other.handle);
+        if (handle)
+            handle.promise().set_owner(&handle);
+        if (other.handle)
+            handle.promise().set_owner(&other.handle);
+    }
+
+    auto operator co_await() & noexcept {
+        return on_coro<Promise, false>{handle};
+    }
+    auto operator co_await() && noexcept {
+        return on_coro<Promise, true>{handle};
+    }
+    bool done() const {
+        return handle.done();
+    }
+
+    ~coro() {
+        if (handle) {
+            handle.destroy();
+        }
+    }
+};
+} // namespace conduit::async
