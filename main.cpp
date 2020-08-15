8aea13334f297378d8d2ab077d084f077397b242
Alecto <perez.cs@pm.me>
2020-08-14 19:30:20 -0600

    Updated main to be example

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
-#include <array>
-#include <conduit/async/get_this_handle.hpp>
-#include <conduit/async/on_suspend.hpp>
-#include <conduit/co_void.hpp>
-#include <conduit/continuation.hpp>
-#include <conduit/future.hpp>
 #include <conduit/source.hpp>
-#include <conduit/task.hpp>
 #include <conduit/void_coro.hpp>
-#include <conduit/generator.hpp>
+#include <conduit/future.hpp>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <vector>
 using namespace conduit;
 
-template <class Coro>
-Coro nums(int min, int inc) {
+source<int> nums(int min, int inc) {
     for (;; min += inc) {
         co_yield min;
     }
 }
-void do_stuff() {
-    auto source = nums<generator<int>>(0, 1);
-    for(int i : source) {
-        printf("%i\n", i);
-        if(i > 100) break;
+source<int> yield0_to_10() {
+    for(int i = 0; i < 10; i++) {
+        co_yield i;
+    }
+}
+optional_future<int> get_future() {
+    co_return 12345;
+}
+void_coro run(auto coro) {
+    int count = 0;
+    while (auto val = co_await coro) {
+        auto value = *val;
+        std::cout << value << '\n';
+        ++count;
+        if (count == 100 || coro.done()) break;
     }
     puts("Done");
 }
 
-int main() { do_stuff(); }
+int main() {
+    run(nums(0, 1));
+    run(yield0_to_10());
+    run(get_future());
+
+    std::cout << "finished\n";
+}
