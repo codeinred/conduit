aa699cf4267fa57ea508563a8fe660d735468cb1
Alecto <perez.cs@pm.me>
2020-08-21 14:20:24 -0600

    Integrated future and source into promise

--- include/conduit/future.hpp
+++ include/conduit/future.hpp
@@ -1,6 +1,33 @@
 #pragma once
 #include <conduit/async/coro.hpp>
-#include <conduit/promise/future.hpp>
+#include <conduit/mixin/promise_parts.hpp>
+#include <conduit/optional_ref.hpp>
+#include <optional>
+
+namespace conduit::promise {
+template <class ReturnValue>
+struct future : mixin::GetReturnObject<future<ReturnValue>>,
+                mixin::HasOwnerAndCallback,
+                mixin::UnhandledException<true> {
+    std::optional<ReturnValue> result;
+    void return_value(ReturnValue&& r) { result.emplace(std::move(r)); }
+    ReturnValue get_value() & { return std::move(*result); }
+    ReturnValue get_value() && { return std::move(*result); }
+};
+
+template <class ReturnValue>
+struct optional_future : mixin::GetReturnObject<optional_future<ReturnValue>>,
+                         mixin::HasOwnerAndCallback,
+                         mixin::UnhandledException<true> {
+    std::optional<ReturnValue> result;
+
+    void return_value(ReturnValue&& r) { result.emplace(std::move(r)); }
+    void return_value(nothing_t) {}
+    std::optional<ReturnValue> get_value() & { return std::move(result); }
+    std::optional<ReturnValue> get_value() && { return std::move(result); }
+};
+} // namespace conduit::promise
+
 
 namespace conduit {
 template<class T>
--- include/conduit/source.hpp
+++ include/conduit/source.hpp
@@ -1,7 +1,46 @@
 #pragma once
 #include <conduit/async/coro.hpp>
-#include <conduit/promise/source.hpp>
-#include <conduit/unique_handle.hpp>
+#include <conduit/mixin/awaitable_parts.hpp>
+#include <conduit/mixin/promise_parts.hpp>
+#include <conduit/optional_ref.hpp>
+
+#include <optional>
+
+namespace conduit::promise {
+template <class ReturnValue>
+struct source : mixin::HasOwnerAndCallback,
+                mixin::GetReturnObject<source<ReturnValue>>,
+                mixin::UnhandledException<true> {
+   private:
+    ReturnValue const* pointer = nullptr;
+
+   public:
+    constexpr auto get_value() & noexcept { return optional_ref{pointer}; }
+    constexpr auto get_value() && noexcept -> std::optional<ReturnValue> {
+        if (pointer) {
+            return std::optional<ReturnValue>{*pointer};
+        } else {
+            return std::nullopt;
+        }
+    }
+    constexpr auto yield_value(ReturnValue const& value) noexcept {
+        pointer = &value;
+        return callback.release();
+    }
+    constexpr void return_void() noexcept { pointer = nullptr; }
+};
+template <>
+struct source<void> : mixin::HasOwnerAndCallback,
+                      mixin::GetReturnObject<source<void>>,
+                      mixin::UnhandledException<true>,
+                      mixin::ReturnVoid {
+   public:
+    auto yield_value(nothing_t) noexcept {
+        return callback.release();
+    }
+};
+} // namespace conduit::promise
+
 
 namespace conduit {
 template<class T>
