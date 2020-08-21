c8d8ecc13662961a0bd44b2a302935bd35c69b50
Alecto <perez.cs@pm.me>
2020-08-20 19:32:50 -0600

    Updated to include test for task

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <conduit/future.hpp>
 #include <conduit/source.hpp>
+#include <conduit/task.hpp>
 #include <conduit/void_coro.hpp>
 #include <cstdio>
 #include <cstdlib>
@@ -8,7 +9,8 @@
 #include <vector>
 using namespace conduit;
 
-#define RUN_TEST(test) std::cout << "Running " #test "...\t" << test() << std::endl;
+#define RUN_TEST(test)                                                         \
+    std::cout << "Running " #test "...\t" << test() << std::endl;
 
 std::string test_void_coro() {
     std::string result = "Coroutine didn't run";
@@ -25,7 +27,7 @@ std::string test_future_1() {
     [&]() -> void_coro {
         int value = 1;
         value = co_await g;
-        if(value == 10) {
+        if (value == 10) {
             result = "Ok";
         } else {
             result = "Incorrect value obtained";
@@ -37,18 +39,32 @@ std::string test_source() {
     std::string result = "Failed";
     [&]() -> void_coro {
         auto g = []() -> source<int> {
-            for(int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {
                 co_yield i;
             }
         }();
         auto expected = std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         std::vector<int> values;
-        while(auto val = co_await g) {
+        while (auto val = co_await g) {
             values.push_back(*val);
         }
-        if(values == expected) {
+        if (values == expected) {
             result = "Ok";
         }
+    }();
+    return result;
+}
+
+std::string test_task() {
+    std::string result = "Failed";
+    [&]() -> void_coro {
+        auto t = [&]() -> task {
+            result = "Ok";
+            co_return;
+        }();
+        co_await t;
+        if (!t.done())
+            result = "Task didn't finish as expected.";
     }();
     return result;
 }
@@ -57,6 +73,6 @@ int main() {
     RUN_TEST(test_void_coro);
     RUN_TEST(test_future_1);
     RUN_TEST(test_source);
-
+    RUN_TEST(test_task); 
     std::cout << "finished\n";
 }
