50dcdd814b9617f63da8b9a8347f035f281e5a0a
Alecto <perez.cs@pm.me>
2020-08-22 00:47:50 -0600

    Ordering + formatting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
+#include <conduit/async/on_suspend.hpp>
 #include <conduit/coroutine.hpp>
 #include <conduit/future.hpp>
 #include <conduit/generator.hpp>
 #include <conduit/recursive_generator.hpp>
 #include <conduit/source.hpp>
 #include <conduit/task.hpp>
-#include <conduit/async/on_suspend.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -32,9 +32,48 @@ future<std::string> test_coroutine(std::string on_success) {
     coro(result);
     co_return result;
 }
+
 future<std::string> test_future(std::string on_success) {
     co_return on_success;
 }
+
+future<std::string> test_generator(std::string on_success) {
+    auto coro = [&]() -> generator<char> {
+        for (char c : on_success) {
+            co_yield c;
+        }
+    };
+
+    auto g = coro();
+
+    co_return std::string(begin(g), end(g));
+}
+
+future<std::string> test_on_suspend(std::string on_success) {
+    std::string result;
+    auto f = [&](std::coroutine_handle<> h, std::string& r) {
+        r = on_success;
+        h.resume();
+    };
+    co_await async::on_suspend(f, fn::bind_last(result));
+    co_return result;
+}
+
+auto example_recursive_generator(std::string_view s)
+    -> recursive_generator<char> {
+    if (s.empty()) {
+        co_return nothing;
+    } else {
+        co_yield s[0];
+        co_return example_recursive_generator(s.substr(1));
+    }
+};
+future<std::string> test_recursive_generator(std::string on_success) {
+    auto g = example_recursive_generator(on_success);
+    co_return std::string(begin(g), end(g));
+}
+
+
 future<std::string> test_source(std::string on_success) {
     auto coro = [&]() -> source<char> {
         for (char c : on_success) {
@@ -63,51 +102,15 @@ future<std::string> test_task(std::string on_success) {
     co_return result;
 }
 
-future<std::string> test_generator(std::string on_success) {
-    auto coro = [&]() -> generator<char> {
-        for (char c : on_success) {
-            co_yield c;
-        }
-    };
-
-    auto g = coro();
-
-    co_return std::string(begin(g), end(g));
-}
-auto example_recursive_generator(std::string_view s)
-    -> recursive_generator<char> {
-    if (s.empty()) {
-        co_return nothing;
-    } else {
-        co_yield s[0];
-        co_return example_recursive_generator(s.substr(1));
-    }
-};
-
-future<std::string> test_recursive_generator(std::string on_success) {
-    auto g = example_recursive_generator(on_success);
-    co_return std::string(begin(g), end(g));
-}
-
-future<std::string> test_on_suspend(std::string on_success) {
-    std::string result;
-    auto f = [&](std::coroutine_handle<> h, std::string& r) {
-        r = on_success;
-        h.resume();
-    };
-    co_await async::on_suspend(f, fn::bind_last(result));
-    co_return result;
-}
 coroutine run_tests() {
     RUN_TEST(test_coroutine);
     RUN_TEST(test_future);
+    RUN_TEST(test_generator);
+    RUN_TEST(test_on_suspend);
+    RUN_TEST(test_recursive_generator);
     RUN_TEST(test_source);
     RUN_TEST(test_task);
-    RUN_TEST(test_generator);
-    RUN_TEST(test_recursive_generator);
-    RUN_TEST(test_on_suspend);
 }
-
 
 int main() {
     run_tests();
