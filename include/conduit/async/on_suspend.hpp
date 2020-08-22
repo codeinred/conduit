6a53ac4bcec6b0ef53abd00136078934096262ff
Alecto <perez.cs@pm.me>
2020-08-22 11:25:43 -0600

    Cleaned make_on_suspend

--- include/conduit/async/on_suspend.hpp
+++ include/conduit/async/on_suspend.hpp
@@ -16,6 +16,7 @@ struct on_suspend : mixin::Resumable<on_suspend<F>> {
     F func;
     std::coroutine_handle<> caller;
     on_suspend(F const& func) : func(func) {}
+    on_suspend(F&& func) : func(std::move(func)) {}
     void set_caller(std::coroutine_handle<> handle) { caller = handle; }
     void resume() { func(caller); }
 };
@@ -25,7 +26,11 @@ on_suspend(F) -> on_suspend<F>;
 
 template <class F, class... Args>
 auto make_on_suspend(F&& f, Args&&... args) {
-    return on_suspend{
-        fn::bind_last(std::forward<F>(f), std::forward<Args>(args)...)};
+    if constexpr (sizeof...(Args) == 0) {
+        return on_suspend(std::forward<F>(f));
+    } else {
+        return on_suspend{
+            fn::bind_last(std::forward<F>(f), std::forward<Args>(args)...)};
+    }
 }
 } // namespace conduit::async
