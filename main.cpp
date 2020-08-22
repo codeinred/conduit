cf29e7f0045cee4f361302b83f8ad3ed981b935d
Alecto <perez.cs@pm.me>
2020-08-21 19:35:54 -0600

    Cleaned up tests

--- main.cpp
+++ main.cpp
@@ -10,69 +10,54 @@
 using namespace conduit;
 
 #define RUN_TEST(test)                                                         \
-    std::cout << "Running " #test "...\t" << test() << std::endl;
+    std::cout << "Running " #test "...\t" << co_await test(#test " succeeded") << std::endl;
 
-std::string test_void_coro() {
-    std::string result = "Coroutine didn't run";
-    [&]() -> void_coro {
-        result = "Ok";
+future<std::string> test_void_coro(std::string on_success) {
+    auto coro = [=](std::string& result) -> void_coro {
+        result = on_success;
         co_return;
-    }();
-    return result;
+    };
+
+    std::string result;
+    coro(result);
+    co_return result;
 }
-std::string test_future_1() {
-    std::string result = "Corutine didn't run";
-    auto f = []() -> future<int> { co_return 10; };
-    auto g = f();
-    [&]() -> void_coro {
-        int value = 1;
-        value = co_await g;
-        if (value == 10) {
-            result = "Ok";
-        } else {
-            result = "Incorrect value obtained";
-        }
-    }();
-    return result;
+future<std::string> test_future(std::string on_success) {
+    co_return on_success;
 }
-std::string test_source() {
-    std::string result = "Failed";
-    [&]() -> void_coro {
-        auto g = []() -> source<int> {
-            for (int i = 0; i < 10; i++) {
-                co_yield i;
-            }
-        }();
-        auto expected = std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        std::vector<int> values;
-        while (auto val = co_await g) {
-            values.push_back(*val);
+future<std::string> test_source(std::string on_success) {
+    auto coro = [=]() -> source<char> {
+        for (char c : on_success) {
+            co_yield c;
         }
-        if (values == expected) {
-            result = "Ok";
-        }
-    }();
-    return result;
+    };
+
+    std::string result;
+    auto source = coro();
+    while(auto c = co_await source) {
+        result += *c;
+    }
+    co_return result;
 }
 
-std::string test_task() {
-    std::string result = "Failed";
-    [&]() -> void_coro {
-        auto t = [&]() -> task {
-            result = "Ok";
-            co_return;
-        }();
-        co_await t;
-        if (!t.done())
-            result = "Task didn't finish as expected.";
-    }();
-    return result;
+future<std::string> test_task(std::string on_success) {
+    auto coro = [=](std::string& s) -> task {
+        s = on_success;
+        co_return;
+    };
+
+    std::string result;
+    co_await coro(result);
+    co_return result;
+}
+void_coro run_tests() {
+    RUN_TEST(test_void_coro);
+    RUN_TEST(test_future);
+    RUN_TEST(test_source);
+    RUN_TEST(test_task);
 }
 
 int main() {
-    RUN_TEST(test_void_coro);
-    RUN_TEST(test_future_1);
-    RUN_TEST(test_source);
-    RUN_TEST(test_task); 
+    run_tests();
     std::cout << "finished\n";
 }
