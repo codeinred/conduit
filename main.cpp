441ded40ba60c3fdbea00bb8b327a56fdcbfa67e
Alecto <perez.cs@pm.me>
2020-08-22 11:44:49 -0600

    Further cleaned on_suspend

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ future<std::string> test_on_suspend(std::string on_success) {
         result = on_success;
         h.resume();
     };
-    co_await async::make_on_suspend(f, result, on_success);
+    co_await async::on_suspend(f, result, on_success);
     co_return result;
 }
 
