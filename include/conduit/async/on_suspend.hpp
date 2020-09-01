cfcbebc47a3b3e35ed6aea23566f9033f13fcb0a
Alecto <perez.cs@pm.me>
2020-08-31 21:39:24 -0600

    Got rid of thing that was breaking MSVC

--- include/conduit/async/on_suspend.hpp
+++ include/conduit/async/on_suspend.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #include <conduit/fn/bind.hpp>
-#include <conduit/mixin/awaitable_parts.hpp>
+#include <conduit/util/stdlib_coroutine.hpp>
 #include <utility>
 
 namespace conduit::async {
 template <class F>
-struct suspend_invoke {
+struct on_suspend {
     [[no_unique_address]] F on_suspend;
 
     constexpr bool await_ready() noexcept { return false; }
@@ -13,16 +13,5 @@ struct suspend_invoke {
     void await_resume() {}
 };
 template <class F>
-suspend_invoke(F) -> suspend_invoke<F>;
-
-template <class F>
-auto on_suspend(F&& f) -> suspend_invoke<F> {
-    return {std::forward<F>(f)};
-}
-template <class F, class... Args>
-auto on_suspend(F&& f, Args&&... args)
-    -> suspend_invoke<decltype(fn::bind_last(std::forward<F>(f),
-                                             std::forward<Args>(args)...))> {
-    return {fn::bind_last(std::forward<F>(f), std::forward<Args>(args)...)};
-}
+on_suspend(F) -> on_suspend<F>;
 } // namespace conduit::async
--- tests/test_on_suspend.cpp
+++ tests/test_on_suspend.cpp
@@ -1,20 +1,13 @@
 #include "run_test.hpp"
-// #if _MSC_VER && !__INTEL_COMPILER
-// // MSVC produces an internal compiler error for on_suspend
-// FAIL_TEST
-// #else
 #include <conduit/async/on_suspend.hpp>
 
 future<std::string> test_on_suspend(std::string on_success) {
     std::string result;
-    auto f = [](std::coroutine_handle<> h, std::string& result,
-                std::string& on_success) {
+    co_await async::on_suspend{[&](std::coroutine_handle<> h) {
         result = on_success;
         h.resume();
-    };
-    co_await async::on_suspend(f, result, on_success);
+    }};
     co_return result;
 }
 
-RUN_CORO_TEST(test_on_suspend);
-// #endif
+RUN_CORO_TEST(test_on_suspend)
