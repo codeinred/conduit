0b8092bb3196876b64a0f4b703fbbbaa23776d83
Alecto <perez.cs@pm.me>
2020-08-31 20:52:40 -0600

    Removed superfluous on_suspend return

--- include/conduit/async/on_suspend.hpp
+++ include/conduit/async/on_suspend.hpp
@@ -9,7 +9,7 @@ template <class F>
 struct suspend_invoke {
     [[no_unique_address]] F on_suspend;
     constexpr bool await_ready() noexcept { return false; }
-    auto await_suspend(std::coroutine_handle<> h) { return on_suspend(h); }
+    auto await_suspend(std::coroutine_handle<> h) { on_suspend(h); }
     void await_resume() {}
 };
 template<class F>
