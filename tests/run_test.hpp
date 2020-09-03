efce0a42087f9548ef39b8078be18139eae8375d
Alecto <perez.cs@pm.me>
2020-09-03 01:02:51 -0600

    Added exception handling to RUN_CORO_TEST

--- tests/run_test.hpp
+++ tests/run_test.hpp
@@ -11,18 +11,29 @@ std::thread alt_thread;
 
 #define RUN_CORO_TEST(test)                                                    \
     int main() {                                                               \
-        []() -> conduit::coroutine {                                           \
+        auto coro = []() -> conduit::coroutine {                               \
             std::string input = "" #test " succeeded";                         \
             auto coro = test(input);                                           \
             auto result = co_await coro;                                       \
             printf("Running test %s...\t", #test);                             \
-            printf("%s\n", result.data());                                       \
-            if (result != input)                                               \
+            printf("%s\n", result.data());                                     \
+            if (result != input) {                                             \
                 std::exit(1);                                                  \
-        }();                                                                   \
+            }                                                                  \
+        };                                                                     \
+        try {                                                                  \
+            coro();                                                            \
+        } catch (std::exception & ex) {                                        \
+            printf("\nException reached main. what(): %s\n", ex.what());       \
+            if (alt_thread.joinable()) {                                       \
+                alt_thread.join();                                             \
+            }                                                                  \
+            return 1;                                                          \
+        }                                                                      \
         if (alt_thread.joinable())                                             \
             alt_thread.join();                                                 \
         return 0;                                                              \
     }
 
-#define FAIL_TEST int main() { return 1; }
+#define FAIL_TEST                                                              \
+    int main() { return 1; }
