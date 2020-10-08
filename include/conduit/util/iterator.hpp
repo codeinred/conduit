a7308f0385e4ecebb80c075f18cb9711ceee7db1
Alecto <perez.cs@pm.me>
2020-10-08 13:09:00 -0600

    iterator stores promise pointer for trivial handles

--- include/conduit/util/iterator.hpp
+++ include/conduit/util/iterator.hpp
@@ -8,6 +8,8 @@ struct coro_sentinal {};
 
 template <class Handle>
 struct coro_iterator {
+    using promise_type =
+        std::decay_t<decltype(std::declval<Handle>().promise())>;
     Handle coro;
 
     // Resumes the coroutine; returns reference to this iterator
@@ -17,7 +19,7 @@ struct coro_iterator {
 
     // Calls deref on the coroutine handle and returns the result
     // By default, this returns coro.promise().current_value;
-    auto const& operator*() noexcept {
+    auto const& operator*() const noexcept {
         if constexpr (has_value_member<promise_type>)
             return coro.promise().value;
         else {
@@ -34,13 +36,54 @@ struct coro_iterator {
     // Returns true iff the coroutine is not done
     bool operator!=(coro_iterator) const noexcept { return !coro.done(); }
 
+    using difference_type = std::ptrdiff_t;
     using iterator_category = std::input_iterator_tag;
-
-    using promise_type = std::decay_t<decltype(coro.promise())>;
     using value_type = std::decay_t<decltype(*std::declval<coro_iterator>())>;
     using pointer = value_type const*;
     using reference = value_type const&;
+
+};
+template <class Promise>
+struct coro_iterator<std::coroutine_handle<Promise>> {
+    using iterator_category = std::input_iterator_tag;
+    using promise_type = Promise;
     using difference_type = std::ptrdiff_t;
+    std::coroutine_handle<Promise> coro;
+    promise_type const* promise;
+
+    coro_iterator() = default;
+    coro_iterator(coro_iterator const&) = default;
+    coro_iterator(std::coroutine_handle<Promise> coro)
+      : coro(coro)
+      , promise(&coro.promise()) {}
+
+    // Resumes the coroutine; returns reference to this iterator
+    void operator++() { coro.resume(); }
+    // Resumes the coroutine
+    void operator++(int) { coro.resume(); }
+
+    // Calls deref on the coroutine handle and returns the result
+    // By default, this returns coro.promise().current_value;
+    auto const& operator*() const noexcept {
+        if constexpr (has_value_member<promise_type>)
+            return promise->value;
+        else {
+            return promise->get_value();
+        }
+    }
+
+    // Returns true iff the coroutine is done
+    bool operator==(coro_sentinal) const noexcept { return coro.done(); }
+    // Returns true iff the coroutine is not done
+    bool operator!=(coro_sentinal) const noexcept { return !coro.done(); }
+
+    bool operator==(coro_iterator) const noexcept { return coro.done(); }
+    // Returns true iff the coroutine is not done
+    bool operator!=(coro_iterator) const noexcept { return !coro.done(); }
+
+    using value_type = std::decay_t<decltype(*std::declval<coro_iterator>())>;
+    using pointer = value_type const*;
+    using reference = value_type const&;
 };
 
 } // namespace conduit
