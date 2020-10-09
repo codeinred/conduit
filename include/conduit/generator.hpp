9018c59285167a28ec311a5d2d745b9d55896ca6
Alecto <perez.cs@pm.me>
2020-10-08 23:44:56 -0600

    Removed vestiges of cppcoro

--- include/conduit/generator.hpp
+++ include/conduit/generator.hpp
@@ -1,44 +1,12 @@
 #pragma once
-///////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Lewis Baker
-// Licenced under MIT license. See LICENSE.txt for details.
-///////////////////////////////////////////////////////////////////////////////
-
-// Copyright 2017 Lewis Baker
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #include <conduit/mixin/promise_parts.hpp>
 #include <conduit/util/iterator.hpp>
 #include <conduit/util/stdlib_coroutine.hpp>
 #include <conduit/util/unique_handle.hpp>
 #include <exception>
-#include <functional>
-#include <iterator>
 #include <type_traits>
-#include <utility>
 
-namespace conduit {
-template <typename T>
-class generator;
-
-namespace promise {
+namespace conduit::promise {
 template <typename T>
 class generator
   : public mixin::InitialSuspend<true>
@@ -59,8 +27,9 @@ class generator
         return coroutine_handle::from_promise(*this);
     }
 };
-} // namespace promise
+} // namespace conduit::promise
 
+namespace conduit {
 template <typename T>
 class [[nodiscard]] generator : unique_handle<promise::generator<T>> {
     using super = unique_handle<promise::generator<T>>;
@@ -98,11 +67,11 @@ class [[nodiscard]] generator : unique_handle<promise::generator<T>> {
 };
 
 template <class T>
-auto begin(generator<T>& gen) {
+auto begin(generator<T> const& gen) {
     return gen.begin();
 }
 template <class T>
-auto end(generator<T>& gen) {
+auto end(generator<T> const& gen) {
     return gen.end();
 }
 } // namespace conduit
