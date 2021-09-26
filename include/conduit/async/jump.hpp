86c7160dbcaa5016c5d96909018ae7a25fd4ff11
Alecto Irene Perez <perez.cs@pm.me>
2021-09-26 17:37:53 -0600

    Updated conduit/async/jump.hpp
    This commit updates the formatting of conduit/async/jump.hpp in
    accordance with the new clang-format file added in commit 738db23d7
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/conduit/async/jump.hpp
+++ include/conduit/async/jump.hpp
@@ -5,9 +5,7 @@ namespace conduit::async {
 struct jump {
     std::coroutine_handle<> callback = nullptr;
     constexpr bool await_ready() noexcept { return false; }
-    auto await_suspend(std::coroutine_handle<>) noexcept {
-        return callback;
-    }
+    auto await_suspend(std::coroutine_handle<>) noexcept { return callback; }
     constexpr void await_resume() noexcept {}
 };
 } // namespace conduit::async
