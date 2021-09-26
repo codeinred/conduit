8e0b6f2f3a5a679fc04b9b46473fc42023ddc4a9
Alecto Irene Perez <perez.cs@pm.me>
2021-09-26 17:37:52 -0600

    Updated conduit/async/callback.hpp
    This commit updates the formatting of conduit/async/callback.hpp in
    accordance with the new clang-format file added in commit 738db23d7
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/conduit/async/callback.hpp
+++ include/conduit/async/callback.hpp
@@ -18,28 +18,27 @@ class callback {
    public:
     using self = callback;
     callback() = default;
-    callback(std::nullptr_t) noexcept : h(nullptr) {}
-    callback(std::coroutine_handle<> h) noexcept : h(h) {}
+    callback(std::nullptr_t) noexcept
+      : h(nullptr) {}
+    callback(std::coroutine_handle<> h) noexcept
+      : h(h) {}
     callback(const callback&) = delete;
-    callback(callback&& source) noexcept : h(release(source.h)) {}
+    callback(callback&& source) noexcept
+      : h(release(source.h)) {}
     // Releases ownership of the coroutine and produces an async::jump
     // with that coroutine
-    [[nodiscard]] inline auto release() noexcept -> jump { return jump{release(h)}; }
-    inline void emplace(std::coroutine_handle<> handle) noexcept {
-        h = handle;
+    [[nodiscard]] inline auto release() noexcept -> jump {
+        return jump {release(h)};
     }
+    inline void emplace(std::coroutine_handle<> handle) noexcept { h = handle; }
 
     inline void swap(callback& other) noexcept {
         auto temp = other.h;
         other.h = h;
         h = temp;
     }
-    inline void resume() {
-        release(h).resume();
-    }
-    inline void operator()() {
-        release(h).resume();
-    }
+    inline void resume() { release(h).resume(); }
+    inline void operator()() { release(h).resume(); }
     ~callback() {
         if (h)
             h.destroy();
